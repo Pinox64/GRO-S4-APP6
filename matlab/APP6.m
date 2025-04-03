ac29b6f62846b1c92e91a3c94de9ce9b7ba107d2
Samuel Hamelin <samuelh1103@hotmail.com>
2025-04-02 20:15:34 -0400

    BA BA BA BAAAAAA
    BÂ BÂ BÂ BÂÂÂÂÂÂ

--- matlab/APP6.m
+++ matlab/APP6.m
@@ -1,15 +1,13 @@
 clear all;
 close all;
-
-[y,Fs] = audioread("note_guitare_LAd.wav");     %Lire le son d'origine
-info = audioinfo("note_guitare_LAd.wav");       
+[y,Fs] = audioread("note_guitare_LAd.wav");
+info = audioinfo("note_guitare_LAd.wav");
 duree_son = info.Duration;
 N = 160000;
 
 w_hamm = hamming(N);
-y_f = y .* w_hamm;      %Appliquer un fenétrage hamming sur le signal d'entrée
+y_f = y .* w_hamm;
 Y = fftshift(fft(y_f));
-
 
 Fmag = abs(Y);
 Fphase = angle(Y);
@@ -48,7 +46,7 @@ plot(n ,Fphase);
 xlabel('Fréquence Hz');
 ylabel('Phase');
 
-%% Passe-bas RIF pour l'enveloppe temporelle
+%% Passe-bas RIF
 
 %Ordre du filtre
 Fc = pi/1000;
@@ -83,21 +81,130 @@ legend('Avant filtrage', 'Après filtrage');
 title('Signal et enveloppe temporelle');
 grid on;
 
-%% Recréer LA#
+%%Recréer LA#
 
-t = 1:160000;
+t = (0:length(y_filtered)-1) / Fs;
+
 sum_sinuses = zeros(1, length(t));
 for i = 1:length(index_harmo)
-    sum_sinuses = sum_sinuses + real(Y(index_harmo(i)))*sin(2*pi*n(index_harmo(i))*t-Fphase(index_harmo(i)));
+    sum_sinuses = sum_sinuses + Fmag(index_harmo(i)) * cos(2*pi*harmoniques(i)*t+Fphase(index_harmo(i)));
 end
 
-synth = sum_sinuses' .* y_filtered;
+synthLA = sum_sinuses' .* y_filtered;
+synthLA = synthLA / max(abs(synthLA));
 
-figure(4);
-plot(synth);
-hold on;
-plot(y);
-hold off;
+%figure(4);
+%plot(synthLA);
+%hold on;
+%plot(y);
+%hold off;
 
-sound(y, Fs);
-%audiowrite('note_guitar_LAd_synthetise.wav', synth, Fs);
+%Synth LA#
+%sound(synthLA, Fs);
+
+%%Recréer SOL
+t = (0:length(y_filtered)-1) / Fs;
+
+SOL_harmoniques = harmoniques*0.891;
+sum_sinuses = zeros(1, length(t));
+for i = 1:length(index_harmo)
+    sum_sinuses = sum_sinuses + Fmag(index_harmo(i)) * cos(2*pi*SOL_harmoniques(i)*t+Fphase(index_harmo(i)));
+end
+
+synthSOL = sum_sinuses' .* y_filtered;
+synthSOL = synthSOL / max(abs(synthSOL));
+
+%T = timer('TimerFcn',@(~,~)disp(''),'StartDelay',4);
+%start(T);
+%wait(T);
+%sound(synthSOL, Fs);
+
+%%Recréer MI
+t = (0:length(y_filtered)-1) / Fs;
+
+MI_harmoniques = harmoniques*0.749;
+sum_sinuses = zeros(1, length(t));
+for i = 1:length(index_harmo)
+    sum_sinuses = sum_sinuses + Fmag(index_harmo(i)) * cos(2*pi*MI_harmoniques(i)*t+Fphase(index_harmo(i)));
+end
+
+synthMI = sum_sinuses' .* y_filtered;
+synthMI = synthMI / max(abs(synthMI));
+
+%T = timer('TimerFcn',@(~,~)disp(''),'StartDelay',4);
+%start(T);
+%wait(T);
+%sound(synthMI, Fs);
+
+%%Recréer FA
+t = (0:length(y_filtered)-1) / Fs;
+
+FA_harmoniques = harmoniques*0.794;
+sum_sinuses = zeros(1, length(t));
+for i = 1:length(index_harmo)
+    sum_sinuses = sum_sinuses + Fmag(index_harmo(i)) * cos(2*pi*FA_harmoniques(i)*t+Fphase(index_harmo(i)));
+end
+
+synthFA = sum_sinuses' .* y_filtered;
+synthFA = synthFA / max(abs(synthFA));
+
+%T = timer('TimerFcn',@(~,~)disp(''),'StartDelay',4);
+%start(T);
+%wait(T);
+%sound(synthFA, Fs);
+
+%%Recréer RE
+t = (0:length(y_filtered)-1) / Fs;
+
+RE_harmoniques = harmoniques*0.667;
+sum_sinuses = zeros(1, length(t));
+for i = 1:length(index_harmo)
+    sum_sinuses = sum_sinuses + Fmag(index_harmo(i)) * cos(2*pi*RE_harmoniques(i)*t+Fphase(index_harmo(i)));
+end
+
+synthRE = sum_sinuses' .* y_filtered;
+synthRE = synthRE / max(abs(synthRE));
+
+%T = timer('TimerFcn',@(~,~)disp(''),'StartDelay',4);
+%start(T);
+%wait(T);
+%sound(synthRE, Fs);
+
+%% Jouer mélodie complète
+
+T = timer('TimerFcn',@(~,~)disp(''),'StartDelay',0.25);
+T2 = timer('TimerFcn',@(~,~)disp(''),'StartDelay',2);
+start(T);
+wait(T);
+stop(T);
+sound(synthSOL, Fs);
+start(T);
+wait(T);
+stop(T);
+sound(synthSOL, Fs);
+start(T);
+wait(T);
+stop(T);
+sound(synthSOL, Fs);
+start(T);
+wait(T);
+stop(T);
+sound(synthMI, Fs);
+start(T2);
+wait(T2);
+stop(T2);
+
+sound(synthFA, Fs);
+start(T);
+wait(T);
+stop(T);
+sound(synthFA, Fs);
+start(T);
+wait(T);
+stop(T);
+sound(synthFA, Fs);
+start(T);
+wait(T);
+stop(T);
+sound(synthRE, Fs);
+
